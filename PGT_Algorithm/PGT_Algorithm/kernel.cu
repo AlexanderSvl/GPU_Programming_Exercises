9720937670f985bdf64c02b1f1eee3cd98792db1
AlexanderSvl <aleksander.svilarovv@gmail.com>
2025-01-14 17:12:16 +0200

    Added the color generation function in the .cu file

--- PGT_Algorithm/PGT_Algorithm/kernel.cu
+++ PGT_Algorithm/PGT_Algorithm/kernel.cu
@@ -24,6 +24,53 @@
 // -- 3. Design an algorithm to integrate the terrain generation
 // -- 4. We need a way to store the previous terrain index, so we can create a gradual terrain generation.
 
+__constant__ float terrain_step = 1.0; // Step by which we are going to modify the terrain.
+
+float4 float_to_color(float x)
+{
+    // Red, green, blue, opacity
+    float4 color;
+
+    if (x >= -10 && x < -5) // Dark blue
+    {
+        color = make_float4(0.0f, 6.0f, 115.0f, 0.8f);
+    }
+    else if (x >= -5 && x < 0) // Light blue
+    {
+        color = make_float4(0.0f, 83.0f, 255.0f, 0.8f);
+    }
+    else if (x >= 0 && x < 5) // Light yellow
+    {
+        color = make_float4(254.0f, 255.0f, 124.0f, 0.8f);
+    }
+    else if (x >= 5 && x < 10) // Dark yellow
+    {
+        color = make_float4(231.0f, 197.0f, 15.0f, 0.8f);
+    }
+    else if (x >= 10 && x < 15) // Light green
+    {
+        color = make_float4(30.0f, 255.0f, 17.0f, 0.8f);
+    }
+    else if (x >= 15 && x < 20) // Dark green
+    {
+        color = make_float4(6.0f, 104.0f, 0.0f, 0.8f);
+    }
+    else if (x >= 20 && x < 25) // Light brown
+    {
+        color = make_float4(196.0f, 164.0f, 132.0f, 0.8f);
+    }
+    else if (x >= 25 && x < 30) // Dark brown
+    {
+        color = make_float4(105.0f, 66.0f, 28.0f, 0.8f);
+    }
+    else // Purple, represent error with the index in the color generation
+    {
+        color = make_float4(191.0f, 0.0f, 137.0f, 0.8f);
+    }
+
+    return color;
+}
+
 __global__ void generate_terrain(float* terrain, int width, int height)
 {
     unsigned int x = blockIdx.x * blockDim.x + threadIdx.x;
@@ -39,7 +86,7 @@ extern "C"
         dim3 blockDim(16, 16);
         dim3 gridDim((width + blockDim.x - 1) / blockDim.x, (height + blockDim.y - 1) / blockDim.y);
 
-        generate_terrain << <gridDim, blockDim >> > (terrain, width, height);
+        generate_terrain <<<gridDim, blockDim >>> (terrain, width, height);
         cudaDeviceSynchronize();
     }
 }
